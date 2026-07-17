a139619cb2a44ba3f9d0510b810fa1afdfc9fb27
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2026-07-17 20:33:51 +0300

    str to int

--- handlers/start.py
+++ handlers/start.py
@@ -8,9 +8,9 @@ router_start = Router()
 @router_start.message(CommandStart())
 async def command_start_handler(message):
     if message.chat.type == "private":
-        telegram_id = str(message.from_user.id)
+        telegram_id = int(message.from_user.id)
     else:
-        telegram_id = str(message.chat.id)
+        telegram_id = int(message.chat.id)
 
     await add_user(telegram_id=telegram_id)
     await message.answer(f"Теперь тебе будут приходить все новости sci.rambler.ru")
